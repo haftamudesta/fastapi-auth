884533367430387cb24a040e5c5c71625fb6c9f5
Haftamu Desta <haftamudesta>
2025-11-23 21:38:45 -0800

    Add fuctionality of adding users to database

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,21 +5,27 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase,relationship
 from datetime import datetime, timezone
-
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
 DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
+from fastapi import Depends
 
 class Base(DeclarativeBase):
     pass
 
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    posts = relationship("Post", back_populates="user")
+
 class Post(Base):
     __tablename__ = "posts"
     id=Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     caption = Column(Text)
     url = Column(String,nullable=False)
     file_type = Column(String,nullable=False)
     file_name = Column(String,nullable=False)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    user = relationship("User", back_populates="posts")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker=async_sessionmaker(engine,expire_on_commit=False)
@@ -30,3 +36,6 @@ async def create_db_and_tables():
 async def get_async_session()->AsyncGenerator[AsyncSession,None]:
     async with async_session_maker() as session:
         yield session
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
