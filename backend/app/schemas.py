245a89f3371ceb1b89f0911084dcf898af28e6cd
Haftamu Desta <haftamudesta>
2025-11-23 22:10:48 -0800

    Add authentication and authorization

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel,Field
+from fastapi_users import schemas
+import uuid
 
 class PostCreate(BaseModel):
     title: str = Field(..., min_length=1, max_length=200, description="Title of the post")
@@ -8,3 +10,12 @@ class PostResponse(BaseModel):
     title:str
     content:str
     author:str
+
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
